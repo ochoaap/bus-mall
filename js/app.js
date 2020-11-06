08f6633d7713c03ab01138bd430ec7e514d9a7fd
Anthony Ochoa <ochoaap@yahoo.com>
2020-11-05 21:34:45 -0800

    creating chart

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ var productArr = [];
 var imageOneEl = document.getElementById('one');
 var imageTwoEl = document.getElementById('two');
 var imageThreeEl = document.getElementById('three');
-
+var ctx = document.getElementById('myChart');
 function Product(name) {
   this.name = name;
   this.src = `image/${name}.jpg`;
@@ -116,3 +116,22 @@ function handleClick(event) {
 container.addEventListener('click', handleClick);
 
 
+
+var chart = new Chart(ctx, {
+  // The type of chart we want to create
+  type: 'line',
+
+  // The data for our dataset
+  data: {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [{
+      label: 'My First dataset',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 5, 2, 20, 30, 45]
+    }]
+  },
+
+  // Configuration options go here
+  options: {}
+});
